6266745effe09545ab391678daa8c71ebc999268
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-15 06:05:46 -0300

    Update --> Index View
    - added a info log.

--- app/front/views/index_view.py
+++ app/front/views/index_view.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views import View
 
+logger = logging.getLogger(__name__)
 
 class IndexView(View):
     def get(self, request):
         template = loader.get_template("pages/index.html")
+        logger.info(f"get template: {template.template.name}")
         return HttpResponse(template.render(None, request))

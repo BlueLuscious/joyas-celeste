7ad50f8f6707f2c994afc9f6da7d32bb68a52468
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-14 21:00:44 -0300

    fixed settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,17 +130,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #     # and creating unique names for each version so they can safely be cached forever.
 #     STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-STATICFILES_DIRS = [BASE_DIR / "globalstatic"]
-
-# # Levanta servidor whitenoise de statics files cuando corre en docker (sino no llegarian los statics)
-# if ENVIRONMENT != "local":
-#     # Tell Django to copy statics to the `staticfiles` directory
-#     # in your application directory on Render.
-#     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-#     # Turn on WhiteNoise storage backend that takes care of compressing static files
-#     # and creating unique names for each version so they can safely be cached forever.
-#     STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

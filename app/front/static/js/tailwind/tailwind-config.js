b622adcc121fdaeab75f442f36f9ad768491bda9
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-16 05:51:35 -0300

    Create --> Tailwind Config

---
+++ app/front/static/js/tailwind/tailwind-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: "Poppins, sans-serif",
+                sans: "Inter, sans-serif",
+            },
+            colors: {
+                primary: {
+                    50: "rgb(233, 250, 255)",
+                    100: "#00ADE6",
+                    600: "#2E9FE5",
+                    700: "#0F3E56",
+                },
+                secondary: {
+                    500: "#1AB71A",
+                },
+            },
+        },
+    },
+};
+
+/* REHACER A GUSTO */

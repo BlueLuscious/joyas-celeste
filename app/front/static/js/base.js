1838af33fc609456df863a5dbf594b3810b95aac
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-16 06:33:37 -0300

    Update--> base.html
    - Created and included right-side-navbar.html to base.html.

--- app/front/static/js/base.js
+++ app/front/static/js/base.js
@@ -2,13 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const menu = document.getElementById("menu")
     const closeMenu = document.getElementById("close_menu")
-    const sideNavbar = document.getElementById("side_navbar")
-    const sideNavbarButtons = [menu, closeMenu]
+    const leftSideNavbar = document.getElementById("left_side_navbar")
+    const leftSideNavbarButtons = [menu, closeMenu]
 
-    sideNavbarButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            sideNavbar.classList.toggle("-translate-x-full")
-        })
-    })
+    openSideNavbar(leftSideNavbarButtons, leftSideNavbar, "-translate-x-full")
+
+    const cart = document.getElementById("cart")
+    const closeCart = document.getElementById("close_cart")
+    const rightSideNavbar = document.getElementById("right_side_navbar")
+    const rightSideNavbarButtons = [cart, closeCart]
+
+    openSideNavbar(rightSideNavbarButtons, rightSideNavbar, "translate-x-full")
 
 })
+
+function openSideNavbar(buttons, element, classlist) {
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            element.classList.toggle(classlist)
+        })
+    })
+}

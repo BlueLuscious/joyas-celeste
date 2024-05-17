d99bda18d05b23051b5105de3db4a4b904e8bc4a
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-17 07:02:32 -0300

    Update --> index.html
    - Added images to category cards.
    - Move hidden labels to recycleBin.txt.

--- app/front/static/js/index.js
+++ app/front/static/js/index.js
@@ -1,21 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const categoryBox = Array.from(document.getElementsByClassName("category_box"))
-    const categoryText = Array.from(document.getElementsByClassName("category_text"))
-    const categoryLabel = Array.from(document.getElementsByClassName("category_label"))
-
-    categoryBox.forEach((box, index) => {
-        box.addEventListener("mouseover", () => {
-            categoryText[index].style.display = "none"
-            categoryLabel[index].classList.toggle("-translate-y-full")
-        })
-    })
-
-    categoryBox.forEach((box, index) => {
-        box.addEventListener("mouseout", () => {
-            categoryText[index].style.display = "block"
-            categoryLabel[index].classList.toggle("-translate-y-full")
-        })
-    })
-
 })

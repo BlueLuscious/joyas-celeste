58fc05837af8df3853503bd7d78e82ca1b38e9e4
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-19 07:31:38 -0300

    Update --> base.html
    - Update left-side-navbar.html --> mock up.
    
    - Created script to expand submenus.

--- app/front/static/js/base.js
+++ app/front/static/js/base.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    /* LEFT SIDE NAVBAR */
+    /* Expand Menu */
     const menu = document.getElementById("menu")
     const closeMenu = document.getElementById("close_menu")
     const leftSideNavbar = document.getElementById("left_side_navbar")
@@ -7,12 +9,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
     openSideNavbar(leftSideNavbarButtons, leftSideNavbar, "-translate-x-full")
 
+    /* Expamd Sub-Menus */
+    const cardArrows = Array.from(document.getElementsByClassName("card_arrow"))
+    const subMenus = Array.from(document.getElementsByClassName("sub_menu"))
+
+    cardArrows.forEach((arrow, index) => {
+        arrow.addEventListener("click", () => {
+            cardArrows[index].classList.toggle("rotate-180")
+            subMenus[index].classList.toggle("h-full")
+            subMenus[index].classList.toggle("opacity-100")
+            subMenus[index].classList.toggle("pb-4")
+        })
+    })
+
+    /* RIGHT SIDE NAVBAR */
     const cart = document.getElementById("cart")
     const closeCart = document.getElementById("close_cart")
     const rightSideNavbar = document.getElementById("right_side_navbar")
     const rightSideNavbarButtons = [cart, closeCart]
 
     openSideNavbar(rightSideNavbarButtons, rightSideNavbar, "translate-x-full")
+
+
 
 })
 

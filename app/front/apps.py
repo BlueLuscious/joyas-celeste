9e0c7a5756b88a53c8980441e58fdd72855accdc
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-22 20:50:31 -0300

    Create --> Category Model & Admin
    - Created CategoryModel.
    - Created CategoryAdmin To CategoryModel.
    - Declared models & admins in apps.py.

--- app/front/apps.py
+++ app/front/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class FrontConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "front"
+
+    def ready(self) -> None:
+        
+        from .admins import (
+            category_admin
+        )
+
+        from .models import (
+            category_model
+        )

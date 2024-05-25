4e706dbe72b1ef14c9e97502f2408c89e9508338
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-25 17:00:28 -0300

    Update --> Admins Front

--- app/front/admins/category_admin.py
+++ app/front/admins/category_admin.py
@@ -5,5 +5,5 @@ from front.models.category_model import CategoryModel
 @admin.register(CategoryModel)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "created_at", "uuid", )
-    ordering = ("created_at", )
+    ordering = ("-created_at", )
     search_fields = ("uuid", "name", )
--- app/front/admins/product_admin.py
+++ app/front/admins/product_admin.py
@@ -4,6 +4,6 @@ from front.models.product_model import ProductModel
 
 @admin.register(ProductModel)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "category", "price", "stock", "created_at", )
-    ordering = ("created_at", )
-    search_fields = ("name", "category__name", )
+    list_display = ("name", "category", "price", "stock", "created_at", "id", )
+    ordering = ("-created_at", )
+    search_fields = ("id", "name", "category__uuid", "category__name", )

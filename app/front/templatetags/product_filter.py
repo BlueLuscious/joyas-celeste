66966658d3a178a8eed96205f46933dced429214
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-06-06 03:23:10 -0300

    Update --> product_filter
    - Add a new filter --> image_or_default.
    - Update type annotations in filters.
    
    - Use filter image_or_default in templates (index.html, category.html & categories.html.)

--- app/front/templatetags/product_filter.py
+++ app/front/templatetags/product_filter.py
@@ -1,18 +1,28 @@
 import locale
+from decimal import Decimal
 from django import template
+from django.db.models.fields.files import ImageFieldFile
+from django.templatetags.static import static
+from front.models.category_model import CategoryModel
 
 register = template.Library()
 
 
 @register.filter
-def products_in_category(products, category):
-    if isinstance(products, dict):
-        return products.get(category, [])
-    else:
-        return [product for product in products if product.category.name == category.name]
+def products_in_category(products: list, category: CategoryModel) -> list:
+    return [product for product in products if product.category.name == category.name]
     
 
 @register.filter
-def format_number(value):
+def format_number(value: Decimal) -> str:
     locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
     return locale.format_string("%.2f", value, grouping=True)
+    
+
+@register.filter
+def image_or_default(image: ImageFieldFile) -> str:
+    if image:
+        url = image.url
+    else:
+        url = static("images/default-no-image.png")
+    return url

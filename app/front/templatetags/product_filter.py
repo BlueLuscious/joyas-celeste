b18f2048fea9dab5ebfad9067575801da5b81cc3
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-06-05 21:11:11 -0300

    Big Update --> styles
    - Add default image --> Find another way by model.
    - Create a filter to format the product price.
    
    - Remove some images from static.

--- app/front/templatetags/product_filter.py
+++ app/front/templatetags/product_filter.py
@@ -1,3 +1,4 @@
+import locale
 from django import template
 
 register = template.Library()
@@ -9,3 +10,9 @@ def products_in_category(products, category):
         return products.get(category, [])
     else:
         return [product for product in products if product.category.name == category.name]
+    
+
+@register.filter
+def format_number(value):
+    locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
+    return locale.format_string("%.2f", value, grouping=True)

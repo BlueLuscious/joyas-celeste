99b4d321097650e35dbf76f4c050bb03143ac121
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-06-23 21:57:32 -0300

    Update --> arrow_icon_tag

--- app/front/templatetags/arrow_icon_tag.py
+++ app/front/templatetags/arrow_icon_tag.py
@@ -5,12 +5,12 @@ logger = logging.getLogger(__name__)
 register = template.Library()
 
 
-@register.inclusion_tag("pages/icons/arrow-down.html")
+@register.inclusion_tag("icons/arrow-down.html")
 def arrow_icon(tailwind_rotate: str = "") -> dict:
     logger.info(f"rotation: {tailwind_rotate}")
     context = {
         "rotate": tailwind_rotate,
     }
-    logger.info(f"product card context: {context}")
+    logger.info(f"tailwind class: {context}")
 
     return context

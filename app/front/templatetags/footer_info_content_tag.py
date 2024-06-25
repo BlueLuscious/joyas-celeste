26748e9c30d326f80561468c4abc8207f7742b32
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-06-25 04:32:44 -0300

    Update --> footer_info_content_tag

--- app/front/templatetags/footer_info_content_tag.py
+++ app/front/templatetags/footer_info_content_tag.py
@@ -7,18 +7,14 @@ register = template.Library()
 
 
 @register.inclusion_tag("pages/components/footer-info-content.html", takes_context=True)
-def footer_info_content(context, id_: str = "", tag_name: str = "", icon_template: str = "", content: str = "") -> dict:
+def footer_info_content(context, id_: str, icon_inclusion: str, content: str) -> dict:
 
-    if icon_template.endswith(".html"):
-        icon_html = context.template.engine.get_template(icon_template).render(context)
-    else:
-        template_string = f"{{% load {tag_name} %}} {{% {icon_template} %}}"
-        icon_html = Template(template_string).render(Context(context.flatten()))
+    template_string = f"{{% include {icon_inclusion} %}}"
+    icon_html = Template(template_string).render(Context(context.flatten()))
 
     context_ = {
         "id": id_,
         "icon_html": icon_html,
-        "tag_name": tag_name,
         "content": content,
     }
     logger.info(f"footer info card context: {context_}")

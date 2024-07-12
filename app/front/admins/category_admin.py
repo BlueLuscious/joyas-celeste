c98e89a7af7461a20d4cbefe1568743cbe0ea3c4
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-07-11 23:39:02 -0300

    Create --> Subcategory Model
    Add --> ForeignKey (Product Model) & ManyToMany (Category Model) to relate it.
    
    Create --> Subcategory Admin.
    Adjust --> Product & Category admins.

--- app/front/admins/category_admin.py
+++ app/front/admins/category_admin.py
@@ -4,6 +4,10 @@ from front.models.category_model import CategoryModel
 
 @admin.register(CategoryModel)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "created_at", "slug", "uuid", )
+    list_display = ("name", "get_subcategories", "slug", "uuid", "created_at", "updated_at", )
     ordering = ("-created_at", )
     search_fields = ("uuid", "name", "slug", )
+
+    def get_subcategories(self, obj):
+        return ", ".join([subcategory.name for subcategory in obj.subcategories.all()])
+    get_subcategories.short_description = "Subcategories"
--- app/front/models/subcategory_model.py
+++ app/front/models/subcategory_model.py
@@ -1,36 +1,29 @@
 from django.db import models
 from django.utils.text import slugify
-from front.models.category_model import CategoryModel
-from front.models.subcategory_model import SubcategoryModel
 from uuid import uuid4
 
 
-class ProductModel(models.Model):
+class SubcategoryModel(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
     name = models.CharField(max_length=128)
     slug = models.SlugField(max_length=128, editable=False, blank=True)
-    category = models.ForeignKey(CategoryModel, on_delete=models.DO_NOTHING)
-    subcategory = models.ForeignKey(SubcategoryModel, on_delete=models.DO_NOTHING)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    stock = models.IntegerField(default=0)
-    image = models.ImageField(upload_to="products", default=None, null=True, blank=True)
     description = models.CharField(max_length=256, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.name
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = slugify(self.name)
-            if ProductModel.objects.filter(slug=slug).exists():
+            if SubcategoryModel.objects.filter(slug=slug).exists():
                 unique_id = str(self.uuid)[:8]
                 slug = f"{slug}-{unique_id}"
             self.slug = slug
-        super(ProductModel, self).save(*args, **kwargs)
+        super(SubcategoryModel, self).save(*args, **kwargs)
         
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["name", "slug"], name="product_name_slug")
+            models.UniqueConstraint(fields=["name", "slug"], name="subcategory_name_slug")
         ]

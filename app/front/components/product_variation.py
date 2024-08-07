efced3f7938a5de11b0b522770f5a11e1945eb3d
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-08-07 06:18:24 -0300

    Update --> Product Variation Component
    - Add component to another templates.
    - Order variations by stock.

--- app/front/components/product_variation.py
+++ app/front/components/product_variation.py
@@ -1,14 +1,17 @@
 from typing import List
 from django_unicorn.components import UnicornView
+from front.models.product_variation_model import ProductVariationModel
 
 
 class ProductVariationView(UnicornView):
+    variations: ProductVariationModel
     product_stock: int
 
     def __init__(self, component_args: List | None = None, **kwargs):
         super().__init__(component_args, **kwargs)
 
-        default_size = self.product.variations.filter(stock__gt=0).first()
+        self.variations = self.product.variations.filter(stock__gt=0).order_by("stock")
+        default_size = self.variations.first()
         if default_size:
             default_size = default_size.measure.size
             self.update_stock(default_size)

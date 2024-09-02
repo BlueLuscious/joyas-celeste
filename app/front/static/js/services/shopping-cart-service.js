95ebd019f25fdfdff713e49ca1762de6c68c8c0d
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-09-02 00:54:58 -0300

    Update --> product variation component
    shopping-cart-service.js:
    - Add --> Conditional to call methods from the component.
    
    product-variation.html:
    - Add --> Conditional to display stock.

--- app/front/static/js/services/shopping-cart-service.js
+++ app/front/static/js/services/shopping-cart-service.js
@@ -8,14 +8,16 @@ export class ShoppingCartService {
     * @returns {void} None.
     */
     static addToCart(product_uuid) {
-        let form = document.getElementById("product_form_" + product_uuid)
+        let form = document.getElementById(`product_form_${product_uuid}`)
         let formData = new FormData(form)
         let product_size = formData.get("product_size")
 
-        Unicorn.call("shopping-cart", "add_to_cart", product_uuid, product_size)
-        setTimeout(() => {
-            Unicorn.call("shopping-cart-counter", "update_cart_counter")
-        }, 100)
+        if (product_size) {
+            Unicorn.call("shopping-cart", "add_to_cart", product_uuid, product_size)
+            setTimeout(() => {
+                Unicorn.call("shopping-cart-counter", "update_cart_counter")
+            }, 100)
+        }
     }
 
 

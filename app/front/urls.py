36b63a07256314cfb6e2dc0388121d6485c5e875
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-12-24 06:16:47 -0300

    Create --> Profile View
    TODO:
    - Enhance --> `profile.html` styles.
    - Create --> Unicorn component to update user fields from profile view.

--- app/front/urls.py
+++ app/front/urls.py
@@ -4,6 +4,7 @@ from front.views.category_view import CategoryView
 from front.views.index_view import IndexView
 from front.views.product_view import ProductView
 from front.views.products_view import ProductsView
+from front.views.profile_view import ProfileView
 from front.views.subcategories_view import SubcategoriesView
 from front.views.subcategory_view import SubcategoryView
 
@@ -16,4 +17,5 @@ urlpatterns = [
     path("category/<str:name>/subcategory/<str:sub_name>/", SubcategoryView.as_view(), name="subcategory"),
     path("products/", ProductsView.as_view(), name="products"),
     path("product/<str:name>/", ProductView.as_view(), name="product"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]

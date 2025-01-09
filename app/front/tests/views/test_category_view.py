ea07acdb1094412d026b189190aa327b88807391
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-01-09 19:52:46 -0300

    Update --> Remove Views & Templates

--- app/front/tests/views/test_category_view.py
+++ app/front/tests/views/test_category_view.py
@@ -11,12 +11,6 @@ class CategoryViewTest(TestCase):
         call_command("loaddata", "seeds/category_seed.json")
 
 
-    def test_get_categories_template(self):
-        response = self.client.get(reverse("categories"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "pages/categories.html")
-
-
     def test_get_category_template(self):
         category = random.choice(CategoryModel.objects.all())
         

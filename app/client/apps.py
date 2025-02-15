0fb31641baac7968503d01c237bf193954990919
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-02-15 11:18:57 -0300

    Change --> Verbose Name

--- app/client/apps.py
+++ app/client/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class ClientConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "client"
-    verbose_name = "Usuarios"
+    verbose_name = "Gestion de Usuarios"
 
     def ready(self) -> None:
         

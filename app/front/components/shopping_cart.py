dcfa9dd6bad92932a5acf89aa930fb0431426788
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-03-04 13:02:59 -0300

    Revert "Test --> Django Messages from Shopping Cart"
    This reverts commit 75408964c4ec7347337d75903389d64caebb4300.

--- app/front/components/shopping_cart.py
+++ app/front/components/shopping_cart.py
@@ -77,8 +77,7 @@ class ShoppingCartView(UnicornView):
             logger.info(f"Add item to cart: {cart_item}")
             self.set_cart()
             self.set_cart_items()
-            messages.success(self.request, "Producto agregado al carrito")
-            self.call("addDjangoMessage")
+            self.call("addMessage", messages.SUCCESS, "Producto agregado al carrito")
         else:
             cart_item = CartItemModel.objects.get(cart=self.cart, key=key)
             if cart_item.quantity < cart_item.stock:
--- app/front/static/js/message.js
+++ app/front/static/js/message.js
@@ -1,6 +1,5 @@
 import { MessageService } from "./services/message-service.js"
 
-window.addDjangoMessage = MessageService.addDjangoMessage
 window.addMessage = MessageService.addMessage
 window.removeMessage = MessageService.removeMessage
 

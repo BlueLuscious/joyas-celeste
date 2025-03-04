55e4870106a2878787c5a8c32a0ebad3f58f9583
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-03-04 18:42:33 -0300

    Clean code -> Front (1)
    - Test changes in Product Variation Component.

--- app/front/components/product_variation.py
+++ app/front/components/product_variation.py
@@ -17,21 +17,19 @@ class ProductVariationView(UnicornView):
         **product_stock (int)**: Stock quantity of product variation.
     """
     
-    product: ProductModel = None
     variations: QuerySet[ProductVariationModel] = ProductVariationModel.objects.none()
     product_stock: int = 0
-    product_size: int = 0
 
     def mount(self) -> None:
 
         """ ProductVariationView First Creation. """
 
+        self.product: ProductModel
         if self.product:
             self.variations = self.product.variations.filter(stock__gt=0).order_by("measure__size")
 
             if self.variations.exists():
                 default_size = self.variations.first().measure.size
-                self.product_size = default_size
                 self.get_stock(default_size)
 
 
@@ -46,11 +44,7 @@ class ProductVariationView(UnicornView):
             size (int): Product size.
         """
 
-        variation = self.product.variations.filter(measure__size=size).first() # TODO: Bug: Sometimes get None
+        variation = self.product.variations.get(measure__size=size) # TODO: Bug: Sometimes get None
         self.product_stock = variation.stock if variation else 0
         logger.info(f"Product variation: {variation} | Variation stock: {self.product_stock}")
-        
-
-    def updated_product_size(self, value: int) -> None:
-        self.get_stock(value)
         

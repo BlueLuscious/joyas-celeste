5ffd46c4d92d35ee490b7c86d3edc173b12c43c3
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-03-10 20:37:21 -0300

    Clean code --> Front (Templates 2)
    Optimize --> Template `index.html` and its related templates, styles & scripts.

--- app/front/static/js/services/shopping-cart-service.js
+++ app/front/static/js/services/shopping-cart-service.js
@@ -3,13 +3,13 @@ export class ShoppingCartService {
     /**
     * Add item to shopping cart:
     * 
-    * Use Unicorn to call methods, add an item to cart and update cart counter.
+    * Use Unicorn to call methods, add an item to cart.
     * @param {string} product_uuid Product UUID.
     * @returns {void} None.
     */
     static addToCart(product_uuid) {
         console.log(`Product UUID: ${product_uuid}`)
-        let product_size = document.getElementById(product_uuid)
+        let product_size = document.getElementById(`product_${product_uuid}_size`)
 
         if (product_size) {
             console.log(`Product Size: ${product_size.value}`)
